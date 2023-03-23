31df928dfa9663c69637c1b49e6f4529adf3fa74
Aaron Tripp <aarontripp83@gmail.com>
2023-03-23 14:13:15 -0700

    component style fix

--- components/BarChart/index.js
+++ components/BarChart/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styles from "./Barchart.module.css";
+import styles from "./BarChart.module.css";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
