561d8c45701159b96e8cdc6a06f454906daee0b2
Aaron Tripp <aarontripp83@gmail.com>
2023-03-23 14:11:32 -0700

    chart fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import BarChart from "@/components/Barchart";
+import BarChart from "@/components/BarChart";
 
 export default function Home() {
   return (
